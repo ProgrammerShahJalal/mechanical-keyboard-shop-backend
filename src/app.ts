e084059ccbe91fbbcfa1510f9db21bddd4023d93
ProgrammerShahJalal <you@example.com>
2024-07-10 15:54:38 +0600

    cors allow

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import express, { Application, Request, Response } from 'express';
 import 'dotenv/config';
 import router from './app/routes';
@@ -8,7 +9,8 @@ import globalErrorHandler from './app/middlewares/globalErrorhandler';
 const app: Application = express();
 //parsers
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 // application routes
 app.use('/api/v1', router);
