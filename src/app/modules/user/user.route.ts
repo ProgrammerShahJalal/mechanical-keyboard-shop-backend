b82bdb3e7d1577eb3613d8671a51af3ef5fbcd00
ProgrammerShahJalal <you@example.com>
2024-07-12 11:59:28 +0600

    issue solved

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 import { UserController } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserValidation } from './user.validation';
-import auth from '../../middlewares/auth';
-import { USER_ROLE } from './user.constant';
 
 const router = Router();
 
@@ -14,21 +12,20 @@ router.post(
 );
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
-//ONLY ADMIN CAN GET ALL USERS
-router.get('/', auth(USER_ROLE.admin), UserController.getAllUsers);
+
+router.get('/', UserController.getAllUsers);
 router.get(
   '/:id',
-  auth(USER_ROLE.admin, USER_ROLE.user),
+
   UserController.getUserById
 );
 router.put(
   '/:id',
-  auth(USER_ROLE.admin, USER_ROLE.user),
-  validateRequest(UserValidation.updateUser),
+
   UserController.updateUser
 );
 router.post('/refresh-token', UserController.refreshToken);
 
-router.delete('/:id', auth(USER_ROLE.admin), UserController.deleteUser);
+router.delete('/:id', UserController.deleteUser);
 
 export const userRouters = router;
